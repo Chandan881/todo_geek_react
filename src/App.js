6aaa52051dc4bad3cc2c4149ea3c55d307da938b
Chandan881 <98389697+Chandan881@users.noreply.github.com>
2023-02-17 12:57:26 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
+
+import {  useState } from 'react';
 import './App.css';
 
+
+
+
+
+
+
 function App() {
+
+const [text, setText] = useState("")
+const [task,setTask] = useState([])
+
+const handleSubmit = (e) => {
+  e.preventDefault();
+  console.log("subited")
+
+  setTask([...task,text]);
+}
+
+
+const deletList = (index) => {
+
+   const finalList = task.filter((el,id)=>{
+      return id !== index;
+    })
+
+    setTask(finalList);
+}
+
+
+
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='container'>
+          <form className='upr' onSubmit={handleSubmit}>
+             <input type="text" className='inpt' placeholder='Enter To Add' value={text} onChange={(e)=>{setText(e.target.value)}} ></input>
+             <button className='btn'>Add</button>
+          </form>
+
+          <div className='showlist'>
+              <div className='row'>
+                  {
+                    task.map((item,index)=>
+                       <div className='list'>
+                          <span className='urs'> {item}</span>
+                          <button className='btns' onClick={()=>{deletList(index)}}>Delete</button>
+                        </div>
+                    )
+                  }
+              </div>
+          </div>
+      </div>
     </div>
   );
 }
